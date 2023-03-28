e4f6228a0c6947b27e93a543b8a8a7fcdb976752
Francisco Javier Caballe <francaballe@gmail.com>
2023-03-28 16:55:55 -0300

    almost done with payments

---
+++ src/controllers/purchaseProduct.js
@@ -0,0 +1,71 @@
+require("dotenv").config();
+const { MELI_TEST_ACCESS_TOKEN } = process.env;
+const { CROSS_ACCESS_TOKEN } = process.env;
+const mercadopago = require("mercadopago");
+
+
+const purchaseProduct = async function (data) {
+
+  const { email, name, token, cart } = data
+
+
+  if (token!==CROSS_ACCESS_TOKEN){
+    throw new Error("Error: token validation did not pass.")
+  }
+
+  mercadopago.configure({
+    access_token: MELI_TEST_ACCESS_TOKEN,
+  });
+  
+    const totalPurchase = [];
+    for (let i = 0; i < cart.length; i++) {
+      totalPurchase.push({
+        title: cart[i].name,
+        unit_price: cart[i].price,
+        quantity: cart[i].quantity,
+        currency_id: "USD"
+      });
+    }
+    //console.log("totalPurchase:",totalPurchase)
+    
+  
+    let preference = {
+      payer: {
+        name: name,
+        email: email,
+      },
+      items: totalPurchase, // por cada producto un item
+      back_urls: {
+        "success": "http://localhost:3000/pokemons",
+        "failure": "http://localhost:3000/pokemons",
+        //"pending": console.log("pending")
+      },
+    };
+  
+    try{
+      const finalPurchase = await mercadopago.preferences.create(preference)
+          .then(function (response) {
+            //onSuccessHandler(response.body.init_point);
+            //res.send(response.body.init_point);                        
+            return response.body.init_point
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+
+      return finalPurchase
+
+    }catch(unError){
+      throw new Error(unError.message)
+    }
+
+
+
+
+
+    
+                   
+      
+  }
+    
+module.exports = purchaseProduct;
--- src/routes/mercadopago.js
+++ src/routes/mercadopago.js
@@ -1,50 +1,17 @@
-require("dotenv").config();
-const { MELI_TEST_ACCESS_TOKEN } = process.env;
 const { Router } = require("express");
 const router = Router();
-const mercadopago = require("mercadopago");
-//const sendMail = require("../controllers/mail/sendMail.js");
+const purchaseProduct = require("../controllers/purchaseProduct")
 
-mercadopago.configure({
-  access_token: MELI_TEST_ACCESS_TOKEN,
-});
 
-router.post("/", async (req, res) => {
-  const { email, name, cart } = req.body;
-  console.log("email, name, cart:",email, name, cart)
-
-  const totalPurchase = [];
-  for (let i = 0; i < cart.length; i++) {
-    totalPurchase.push({
-      title: "poke_sobre",//cart[i].name,
-      unit_price: cart[i].price,
-      quantity: cart[i].quantity,
-    });
+router.post("/", async function (req, res){
+        
+  try{
+    let respuesta = await purchaseProduct(req.body);    
+    res.send(respuesta);
+  }catch(unError){
+    res.status(400).send(unError.message);
   }
-  console.log("totalPurchase:",totalPurchase)
 
-  let preference = {
-    payer: {
-      name: "francisquito",
-      email: "francaballe@gmail.com",
-    },
-    items: totalPurchase, // por cada producto un item
-    back_urls: {
-      "success": "http://localhost:3000",
-      "failure": "http://localhost:3000",
-      //"pending": console.log("pending")
-    },
-  };
-
-  mercadopago.preferences
-    .create(preference)
-    .then(function (response) {
-      //onSuccessHandler(response.body.init_point);
-      res.send(response.body.init_point);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
 });
 
 module.exports = router;
